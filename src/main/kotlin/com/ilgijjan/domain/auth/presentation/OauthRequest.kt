b4ad46c45389dd04e8d00e7c601adcbca7708391
yueunfive <122276414+yueunfive@users.noreply.github.com>
2026-01-04 20:23:28 +0900

    fix: @ValidOauthRequest 활성화

--- src/main/kotlin/com/ilgijjan/domain/auth/presentation/OauthRequest.kt
+++ src/main/kotlin/com/ilgijjan/domain/auth/presentation/OauthRequest.kt
@@ -3,6 +3,7 @@ package com.ilgijjan.domain.auth.presentation
 import com.ilgijjan.domain.auth.domain.OauthProvider
 import io.swagger.v3.oas.annotations.media.Schema
 
+@ValidOauthRequest
 interface OauthRequest {
     @get:Schema(description = "소셜 로그인 제공자 (KAKAO)", nullable = false, example = "KAKAO")
     val provider: OauthProvider
