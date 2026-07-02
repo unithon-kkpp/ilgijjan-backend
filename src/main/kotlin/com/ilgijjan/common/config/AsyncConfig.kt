578acfdf5f4d766dec1586ae51cdfbc04da90185
yueunfive <122276414+yueunfive@users.noreply.github.com>
2026-07-02 15:23:00 +0900

    chore: 불필요한 import문 제거

--- src/main/kotlin/com/ilgijjan/common/config/AsyncConfig.kt
+++ src/main/kotlin/com/ilgijjan/common/config/AsyncConfig.kt
@@ -1,6 +1,5 @@
 package com.ilgijjan.common.config
 
-import com.ilgijjan.common.log.MdcTaskDecorator
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableAsync
